750551bfa04d8f2b45fb04bde1b7ab1c3bd039d0
vmuchui <kaigongimuchui@gmail.com>
2019-03-22 11:30:46 +0300

    this commit changes the name of the function from side to track.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var sideA = parseFloat(prompt("Enter first value!"));
 var sideB = parseFloat(prompt("Enter second value!"));
 var sideC = parseFloat(prompt("Enter third value!"));
-function side(a,b,c){
+var track = function(a,b,c){
 return a + b > c && a + c > b && b + c > a
-};
+}
