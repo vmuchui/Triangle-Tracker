37298d7eef7683cf5d83633f678ea4f025524e67
vmuchui <kaigongimuchui@gmail.com>
2019-03-22 11:54:33 +0300

    this commit adds the if-else & else if functions to the javascript file!

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ var sideC = parseFloat(prompt("Enter third value!"));
 var track = function(a,b,c){
 return a + b > c && a + c > b && b + c > a
 }
+if(track(sideA,sideB,sideC)&& sideA===sideB && sideB===sideC && sideA===sideC){
+alert("Your triangle is equilateral!");
+}
+else if(track(sideA,sideB,sideC)&& sideA===sideB && sideA!==sideC || sideB===sideC && sideB!==sideA || sideA===sideC && sideC!==sideB){
+alert("Your triangle is Isosceles!")
+}
+else if(track(sideA,sideB,sideC)&& sideA!==sideB && sideB!==sideC && sideA!==sideC){
+alert("Your triangle is Scalene!")
+}
+else {
+alert("Your values do not create a triangle!")
+}
